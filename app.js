b8c2611bf55ecb8522b14eed08702ef6664a349b
yusra-sabie05 <yusrasabie185@gmail.com>
2024-04-07 02:09:04 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "please guess a number",
+    },
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("Congratulations you guessed a right number");
+}
+else {
+    console.log("you guessed a wrong number");
+}
